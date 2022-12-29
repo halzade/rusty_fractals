391848c6d76d632e38b70775e63a8b19eb690f90
lukas <lukas@halza.de>
2022-12-29 18:55:38 +0100

    add rusty_fractals_common

--- rusty_fractals_core/src/lib.rs
+++ rusty_fractals_core/src/lib.rs
@@ -2,7 +2,6 @@ mod engine;
 pub mod machine;
 pub mod fractal;
 pub mod fractal_stats;
-mod target;
 pub mod mathematician;
 pub mod mem;
 pub mod mem_collatz;
--- rusty_fractals_domain/src/lib.rs
+++ rusty_fractals_domain/src/lib.rs
@@ -1,6 +1,5 @@
 mod domain_pixel;
 pub mod domain;
-pub mod domain_area;
 pub mod domain_element;
 pub mod resolution_multiplier;
 mod domain_pixel_colors;
