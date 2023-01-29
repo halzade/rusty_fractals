00e0e5c644a46a6c83fb45a359902171b9b8aff7
lukas <lukas@halza.de>
2023-01-29 02:08:40 +0100

    update nebula top fractal

--- fractal_nebula_top/src/main.rs
+++ fractal_nebula_top/src/main.rs
@@ -1,27 +1,16 @@
-use rusty_fractals_result::palette::Palette;
+use rusty_fractals_core::{machine, window};
+use rusty_fractals_common::area;
+use rusty_fractals_common::mem::Mem;
+use rusty_fractals_common::fractal::{AppConfig, CalculationConfig, Math};
+use rusty_fractals_common::resolution_multiplier::ResolutionMultiplier::Square2;
+use rusty_fractals_domain::{domain};
 use rusty_fractals_result::palettes::palette_purple_to_white;
-use rusty_fractals_core::mem::Mem;
-use rusty_fractals_core::fractal::{FractalConfig, FractalDefinition, Math};
-use rusty_fractals_domain::resolution_multiplier;
-use resolution_multiplier::ResolutionMultiplier;
-use resolution_multiplier::ResolutionMultiplier::SquareAlter;
+use rusty_fractals_result::result::ResultConfig;
 
-const NAME: &str = "Nebula top";
-const ITERATION_MAX: u32 = 14800;
-const ITERATION_MIN: u32 = 42;
-const AREA_SIZE: f64 = 6.0;
 const TARGET_RE: f64 = -1.40115859004747;
 const TARGET_IM: f64 = -0.00000000709356;
-const RESOLUTION_WIDTH: u32 = 1920;
-const RESOLUTION_HEIGHT: u32 = 1080;
-const RESOLUTION_MULTIPLIER: ResolutionMultiplier = SquareAlter;
-const REPEAT: bool = true;
-const SAVE_IMAGES: bool = false;
-const PALETTE: Palette = palette_purple_to_white();
 
-struct NebulaTop {
-    pub name: String,
-}
+struct NebulaTop {}
 
 impl Math<Mem> for NebulaTop {
     fn math(&self, m: &mut Mem, origin_re: f64, origin_im: f64) {
@@ -31,24 +20,47 @@ impl Math<Mem> for NebulaTop {
 }
 
 fn main() {
-    println!("Started");
+    let name = "Nebula Top";
 
-    let nebula = NebulaTop { name: NAME.to_string() };
-    let definition = FractalDefinition { iteration_min: ITERATION_MIN, iteration_max: ITERATION_MAX, area_size: AREA_SIZE, target_re: TARGET_RE, target_im: TARGET_IM };
-    let config = FractalConfig { resolution_width: RESOLUTION_WIDTH, resolution_height: RESOLUTION_HEIGHT, resolution_multiplier: RESOLUTION_MULTIPLIER, repeat: REPEAT, save_images: SAVE_IMAGES, palette: PALETTE };
+    const WIDTH: usize = 800; // 1920
+    const HEIGHT: usize = 800; // 1080
 
-    println!("Fractal {}", nebula.name);
+    let calculation_config = CalculationConfig {
+        iteration_min: 42,
+        iteration_max: 14800,
+        resolution_multiplier: Square2,
+    };
+    let app_config = AppConfig {
+        repeat: true,
+        save_images: false,
+    };
+    let area_cfg = area::AreaConfig {
+        width_re: 6.0,
+        center_re: TARGET_RE,
+        center_im: TARGET_IM,
+        width_x: WIDTH,
+        height_y: HEIGHT,
+    };
+    let result_config = ResultConfig {
+        palette: palette_purple_to_white(),
+    };
 
-    let mut m = Mem { re: 0.0, im: 0.0 };
-    nebula.math(&mut m, 1.0, 0.1);
+    println!("Fractal {}", name);
 
-    println!("Finished.");
+    let nebula_top = NebulaTop {};
+    let area = area::init(&area_cfg);
+    let domain = domain::init(&area);
+    let machine = machine::init(&calculation_config, &app_config, &result_config);
+
+    let (domain_image, result_image) = machine.calculate(&nebula_top, &domain, &area);
+
+    window::show(name, domain_image, result_image);
 }
 
 
 #[test]
 fn test_math() {
-    let nebula = NebulaTop { name: NAME.to_string() };
+    let nebula = NebulaTop {};
     let mut m = Mem { re: 0.0, im: 0.0 };
     nebula.math(&mut m, 1.0, 0.1);
     assert_eq!(m.re, 1.0);
