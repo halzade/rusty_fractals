5021c76669864834e29968d889173400eb5b2ab6
lukas <lukas@halza.de>
2023-02-22 10:39:42 +0100

    Remove rusty_fractals_result and rusty_fractals_domain projects

--- rusty_fractals_domain/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod domain;
-pub mod pixel_states;
-pub mod domain_element;
--- rusty_fractals_result/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod palette;
-pub mod palettes;
-pub mod palette_utils;
-pub mod result;
-pub mod result_pixels;
-pub mod result_element_static;
-pub mod perfect_color_distribution;
