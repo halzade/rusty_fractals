31450651b69b9905a086c704f4466b958a44a5cd
lukas <lukas@halza.de>
2023-02-24 09:31:46 +0100

    better init palettes

--- rusty_fractals_common/src/palettes.rs
+++ rusty_fractals_common/src/palettes.rs
@@ -10,21 +10,17 @@ const RED: Rgb<u8> = Rgb([255, 0, 0]);
 const GREEN: Rgb<u8> = Rgb([0, 255, 0]);
 const BLUE: Rgb<u8> = Rgb([0, 0, 255]);
 
-pub struct ResultConfig {
-    pub palette: Palette,
-}
-
-pub struct ResultConfigMandelbrot {
-    pub palette: Palette,
-    // to color insides of mandelbrot set
-    pub palette_zero: Palette,
-}
-
 pub enum Function { Linear1, Linear3, Linear7, Quadratic, Exp, Exp2, CircleDown, CircleUp }
 
 pub fn palette_black_to_white_exp2() -> Palette {
     Palette {
         spectrum: make_spectrum(Exp2, BLACK, WHITE)
+    }
+}
+
+pub fn new(function: Function, from: Rgb<u8>, to: Rgb<u8>) -> Palette {
+    Palette {
+        spectrum: make_spectrum(function, from, to)
     }
 }
 
@@ -38,21 +34,15 @@ pub fn palette_bwb() -> Palette {
 }
 
 pub fn palette_blue_to_white_circle_up() -> Palette {
-    Palette {
-        spectrum: make_spectrum(CircleUp, Rgb([4, 13, 33]), WHITE)
-    }
+    new(CircleUp, Rgb([4, 13, 33]), WHITE)
 }
 
 pub fn palette_gray_to_blue() -> Palette {
-    Palette {
-        spectrum: make_spectrum(CircleDown, Rgb([104, 113, 133]), Rgb([4, 13, 33]))
-    }
+    new(CircleDown, Rgb([104, 113, 133]), Rgb([4, 13, 33]))
 }
 
 pub fn palette_purple_to_white() -> Palette {
-    Palette {
-        spectrum: make_spectrum(CircleUp, Rgb([20, 3, 30]), WHITE)
-    }
+    new(CircleUp, Rgb([20, 3, 30]), WHITE)
 }
 
 pub fn palette_3_rgb() -> Palette3 {
