0b16a51e2ed680a67c2ae5a436a7c36b0ca7dbe4
lukas <lukas@halza.de>
2023-02-27 23:11:49 +0100

    zoom in nebula_of_nebula

--- fractal_nebula_of_nebula/src/main.rs
+++ fractal_nebula_of_nebula/src/main.rs
@@ -2,10 +2,10 @@ use rusty_fractals_core::{machine};
 use rusty_fractals_common::area::{Area, AreaConfig};
 use rusty_fractals_common::data_image::DataImage;
 use rusty_fractals_common::mem::Mem;
-use rusty_fractals_common::fractal::{FractalConfig, Fractal, FractalMath, Update, Conf};
+use rusty_fractals_common::fractal::{FractalConfig, Fractal, FractalMath};
 use rusty_fractals_common::fractal;
 use rusty_fractals_common::palettes::palette_blue_to_white_circle_up;
-use rusty_fractals_common::resolution_multiplier::ResolutionMultiplier::{Square2, Square9};
+use rusty_fractals_common::resolution_multiplier::ResolutionMultiplier::Square3;
 
 struct NebulaOfNebula {
     name: &'static str,
@@ -28,7 +28,7 @@ impl FractalMath<Mem> for NebulaOfNebula {
 
 impl Fractal for NebulaOfNebula {
     fn path_test(&self, min: u32, max: u32, length: u32, iterator: u32) -> bool {
-        fractal::infinite_orbits(min, max, length, iterator)
+        fractal::finite_orbits(min, max, length, iterator)
     }
     fn calculate_path(&self, area: &Area, iteration_min: u32, iteration_max: u32, origin_re: f64, origin_im: f64, data: &DataImage, is_wrap: bool) -> (u32, u32) {
         fractal::calculate_path(self, self, area, iteration_min, iteration_max, origin_re, origin_im, data, is_wrap)
@@ -42,13 +42,13 @@ fn main() {
     let fractal_config = FractalConfig {
         iteration_min: 42,
         iteration_max: 24800,
-        resolution_multiplier: Square2,
+        resolution_multiplier: Square3,
         palette: palette_blue_to_white_circle_up(),
     };
     let area_config = AreaConfig {
         width_x: 1280,
         height_y: 1000,
-        width_re: 1.5,
+        width_re: 0.5,
         center_re: 0.0,
         center_im: 0.0,
     };
