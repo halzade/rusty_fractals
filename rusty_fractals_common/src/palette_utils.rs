905e0784f0e9f6a48c8c62dd8246ed35c6cf4daa
lukas <lukas@halza.de>
2023-02-27 22:41:31 +0100

    fix make_spectrum() and add color palette certification

--- rusty_fractals_common/src/palette_utils.rs
+++ rusty_fractals_common/src/palette_utils.rs
@@ -3,7 +3,7 @@ use palettes::Function;
 
 use crate::palettes;
 
-fn max(r: i16, g: i16, b: i16) -> i16 {
+fn max(r: i32, g: i32, b: i32) -> i32 {
     if a(r) >= a(g) && a(r) >= a(b) {
         return r;
     } else if a(g) >= a(r) && a(g) >= a(b) {
@@ -14,8 +14,8 @@ fn max(r: i16, g: i16, b: i16) -> i16 {
     panic!()
 }
 
-fn a(v: i16) -> i16 {
-    v.abs()
+fn a(v: i32) -> u8 {
+    v.abs() as u8
 }
 
 // Fill colour spectrum with colours between colours:
@@ -23,43 +23,57 @@ fn a(v: i16) -> i16 {
 // to       : colour for higher values
 // function : defines gradient of colour change
 pub fn make_spectrum(function: Function, from: Rgb<u8>, to: Rgb<u8>) -> Vec<Rgb<u8>> {
-    let r_from = from.channels()[0] as i16;
-    let g_from = from.channels()[1] as i16;
-    let b_from = from.channels()[2] as i16;
+    let r_from = from.channels()[0];
+    let g_from = from.channels()[1];
+    let b_from = from.channels()[2];
 
-    let r_to = to.channels()[0] as i16;
-    let g_to = to.channels()[1] as i16;
-    let b_to = to.channels()[2] as i16;
+    let r_to = to.channels()[0];
+    let g_to = to.channels()[1];
+    let b_to = to.channels()[2];
 
-    let r_dif = r_to - r_from;
-    let g_dif = g_to - g_from;
-    let b_dif = b_to - b_from;
+    // result may be negative number
+    let r_dif = r_to as i32 - r_from as i32;
+    let g_dif = g_to as i32 - g_from as i32;
+    let b_dif = b_to as i32 - b_from as i32;
 
     println!("rgb from {} {} {} ", r_from, g_from, b_from);
     println!("rgb to   {} {} {} ", r_to, g_to, b_to);
     println!("rgb dif  {} {} {} ", r_dif, g_dif, b_dif);
 
-    let max_dif = a(max(r_dif, g_dif, b_dif)) as f64;
+    let max_dif = max(r_dif, g_dif, b_dif) as f64;
+    let max_dif_abs = a(max_dif as i32);
+    println!("max dif: {}", max_dif);
 
-    let r_step: f64 = (r_dif as f64 / max_dif) as f64;
-    let g_step: f64 = (g_dif as f64 / max_dif) as f64;
-    let b_step: f64 = (b_dif as f64 / max_dif) as f64;
+    let r_step: f64 = (r_dif as f64 / max_dif_abs as f64) as f64;
+    let g_step: f64 = (g_dif as f64 / max_dif_abs as f64) as f64;
+    let b_step: f64 = (b_dif as f64 / max_dif_abs as f64) as f64;
+    println!("step r: {}", r_step);
+    println!("step g: {}", g_step);
+    println!("step b: {}", b_step);
 
     let mut stop = false;
 
     let rgb255 = 255.0;
 
     let mut spectrum: Vec<Rgb<u8>> = Vec::new();
+    let forward = function_result(0.0, &function) != 1.0;
+    println!("spectrum forward: {}", forward);
 
-    for i in 0..a(max_dif as i16) {
+    for i in 0..max_dif_abs + 1 {
         let d: f64 = (i as f64 / max_dif) as f64;
         // optimized dif on interval <0, 1>
+        // 0 -> 1, like circle up is forward
+        // 1 -> 0, like circle down is backwards
         let v: f64 = function_result(d, &function);
-        let value = v * max_dif;
+        let value = v * max_dif_abs as f64;
 
         let mut r_new = r_from as f64 + (value * r_step);
         let mut g_new = g_from as f64 + (value * g_step);
         let mut b_new = b_from as f64 + (value * b_step);
+        if i == 0 {
+            println!("v: {}", v);
+            println!("r_new={} = {} + ({} * {})", r_new, r_from, value, r_step);
+        }
 
         if r_new > rgb255 {
             r_new = rgb255;
@@ -106,12 +120,36 @@ pub fn make_spectrum(function: Function, from: Rgb<u8>, to: Rgb<u8>) -> Vec<Rgb<
 
         // Add colours to Palette
         spectrum.push(Rgb([r_new as u8, g_new as u8, b_new as u8]));
-
+        // println!("{} {} {}", r_new as u8, g_new as u8, b_new as u8);
         if stop {
             break;
         }
     }
-    assert_eq!(spectrum.len(), max_dif as usize);
+
+    let spec_from;
+    let spec_to;
+    if forward {
+        spec_from = spectrum.get(0).unwrap();
+        spec_to = spectrum.get(spectrum.len() - 1).unwrap();
+    } else {
+        // functions (1 -> 0) like circle down
+        spec_from = spectrum.get(spectrum.len() - 1).unwrap();
+        spec_to = spectrum.get(0).unwrap();
+    }
+    let spec_r_from = spec_from.channels()[0] as u8;
+    let spec_g_from = spec_from.channels()[1] as u8;
+    let spec_b_from = spec_from.channels()[2] as u8;
+    let spec_r_to = spec_to.channels()[0] as u8;
+    let spec_g_to = spec_to.channels()[1] as u8;
+    let spec_b_to = spec_to.channels()[2] as u8;
+    // assert spectrum so that the coloring can be perfect
+    assert_eq!(r_from, spec_r_from);
+    assert_eq!(g_from, spec_g_from);
+    assert_eq!(b_from, spec_b_from);
+    assert_eq!(r_to, spec_r_to);
+    assert_eq!(g_to, spec_g_to);
+    assert_eq!(b_to, spec_b_to);
+    println!("color spectrum certified");
     spectrum
 }
 
