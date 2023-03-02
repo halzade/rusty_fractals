11d7baa10fad36473b978bc2f5142e6bb719afbf
lukas <lukas@halza.de>
2023-03-02 13:59:45 +0100

    add fractal specific log stub

---
+++ rusty_fractals_common/src/fractal_log.rs
@@ -0,0 +1,23 @@
+use chrono::Utc;
+
+const TIME_FORMAT: &str = "%Y.%m.%d %H:%M:%S.%F";
+
+pub fn now(message: &str) {
+    println!("{}: {}", no(), message);
+}
+
+const X: usize = 249;
+const Y: usize = 248;
+
+pub fn only(x: usize, y: usize, message: &str) {
+    if x == X {
+        if y == Y {
+            println!("* {}: {}", no(), message);
+        }
+    }
+}
+
+fn no() -> String {
+    Utc::now().format(TIME_FORMAT).to_string()
+}
+
