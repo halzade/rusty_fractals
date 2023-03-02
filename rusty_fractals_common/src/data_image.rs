9cf6a4d146781f0bdf387ee789026d4b917fe946
lukas <lukas@halza.de>
2023-03-02 14:32:04 +0100

    recalculate_pixels_positions_for_next_calculation() speed up 100x

--- rusty_fractals_common/src/data_image.rs
+++ rusty_fractals_common/src/data_image.rs
@@ -5,6 +5,7 @@ use crate::area::Area;
 use crate::constants::{GRAY, MINIMUM_PATH_LENGTH, NEIGHBOURS};
 use crate::{area, data_px};
 use crate::data_px::DataPx;
+use crate::fractal_log::now;
 use crate::pixel_states::{ACTIVE_NEW, DomainElementState, FINISHED_SUCCESS, FINISHED_SUCCESS_PAST, FINISHED_TOO_LONG, FINISHED_TOO_SHORT, HIBERNATED_DEEP_BLACK, is_finished_any, is_finished_success_past};
 use crate::pixel_states::DomainElementState::{ActiveNew, FinishedSuccess, FinishedSuccessPast, FinishedTooLong, FinishedTooShort, HibernatedDeepBlack};
 use crate::resolution_multiplier::ResolutionMultiplier;
@@ -15,7 +16,7 @@ pub struct DataImage {
     pub height: usize,
     pub dynamic: bool,
     // static data for image
-    pub pixels: Vec<Vec<Arc<Mutex<DataPx>>>>,
+    pub pixels: Vec<Vec<Mutex<Option<DataPx>>>>,
     // dynamic data for zoom video
     // As zoom progress, points [re,im] are projected to new pixels [px,py] until they migrate out of the the tiny area.
     // Elements outside of tiny result_area are removed. Very short (calculation) paths are also removed.
@@ -30,7 +31,8 @@ static MAX_VALUE: Mutex<u32> = Mutex::new(0);
 
 impl DataImage {
     pub fn colour(&self, x: usize, y: usize, palette_colour: Rgb<u8>) {
-        let mut p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         p.colour = Some(palette_colour);
     }
 
@@ -144,64 +146,61 @@ impl DataImage {
     }
 
     fn add(&self, x: usize, y: usize) {
-        let mut p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         p.value += 1;
     }
 
-    fn mpx_at(&self, x: usize, y: usize) -> MutexGuard<DataPx> {
-        let arc_mutex_dpx = self.pixels.get(x).unwrap().get(y).unwrap();
-        arc_mutex_dpx.lock().unwrap()
+    fn mo_px_at(&self, x: usize, y: usize) -> MutexGuard<Option<DataPx>> {
+        self.pixels.get(x).unwrap().get(y).unwrap().lock().unwrap()
     }
 
-    fn mpx_o_at(&self, x: usize, y: usize) -> Option<&Arc<Mutex<DataPx>>> {
-        // let arc_mutex_dpx = self.pixels.get(x).unwrap().get(y).unwrap();
-        let ovx = self.pixels.get(x);
-        match ovx {
-            None => { None }
-            Some(vx) => {
-                let ovy = vx.get(y);
-                match ovy {
-                    None => { None }
-                    Some(vy) => {
-                        return Some(vy);
-                    }
-                }
+    fn move_px_to_new_position(&self, x: usize, y: usize, px: DataPx) {
+        let mu = self.pixels.get(x).unwrap().get(y).unwrap();
+        let lo = mu.lock();
+        match lo {
+            Ok(mut op) => {
+                op.replace(px);
+            }
+            Err(e) => {
+                println!("move_px_to_new_position(): {}", e);
             }
         }
     }
 
-    fn replace_px(&self, x: usize, y: usize, px_clone: DataPx) {
-        let vy = self.pixels.get(x).unwrap().to_owned();
-        *vy[y].lock().unwrap() = px_clone;
-    }
-
     pub fn values_at(&self, x: usize, y: usize) -> (u32, DomainElementState, f64, f64, Option<Rgb<u8>>) {
-        let p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         (p.value, p.state, p.quad, p.quid, p.colour)
     }
 
     pub fn value_state_at(&self, x: usize, y: usize) -> (u32, DomainElementState) {
-        let p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         (p.value, p.state)
     }
 
     pub fn value_at(&self, x: usize, y: usize) -> u32 {
-        let p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         p.value
     }
 
     pub fn state_origin_at(&self, x: usize, y: usize) -> (DomainElementState, f64, f64) {
-        let p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         (p.state, p.origin_re, p.origin_im)
     }
 
     pub fn origin_at(&self, x: usize, y: usize) -> (f64, f64) {
-        let p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         (p.origin_re, p.origin_im)
     }
 
     pub fn set_pixel_mandelbrot(&self, x: usize, y: usize, iterator: u32, quad: f64, state: DomainElementState, max: u32) {
-        let mut p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         p.quad = quad;
         p.quid = 1.0 / quad;
         p.state = state;
@@ -216,7 +215,8 @@ impl DataImage {
 
     // for Nebula like fractals
     pub fn set_pixel_state(&self, x: usize, y: usize, state: DomainElementState) {
-        let mut p = self.mpx_at(x, y);
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
         p.quad = 1.0;
         p.quid = 1.0;
         p.state = state;
@@ -226,7 +226,9 @@ impl DataImage {
         println!("recalculate_pixels_states()");
         for y in 0..self.height {
             for x in 0..self.width {
-                self.mpx_at(x, y).past();
+                let mut mo_px = self.mo_px_at(x, y);
+                let p = mo_px.as_mut().unwrap();
+                p.past();
             }
         }
     }
@@ -282,7 +284,7 @@ impl DataImage {
         let mut sum = 0;
         for x in x_from..x_to {
             for y in y_from..y_to {
-                sum += self.mpx_at(x, y).value;
+                sum += self.mo_px_at(x, y).as_ref().unwrap().value;
             }
         }
         sum
@@ -320,123 +322,101 @@ impl DataImage {
         // Scan all elements : old positions from previous calculation
         // Some elements will be moved to new positions
         // For all the moved elements, subsequent calculations will be skipped.
-        // 1. save references for elements moving to now positions
-        let mut c_to_move = 0;
-        let mut c_to_drop = 0;
-        let mut px_for_move: Vec<DataPx> = Vec::new();
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let px = self.mpx_at(x as usize, y as usize);
-                // There was already zoom in, the new area is smaller
-                if area.contains(px.origin_re, px.origin_im) {
-                    // Element did not move out of the zoomed in area
-                    c_to_move += 1;
-                    px_for_move.push(px.clone());
-                } else {
-                    c_to_drop += 1;
-                }
+        let (cx, cy) = area.point_to_pixel(area.center_re, area.center_im);
+        now("1. move top left to center");
+        for y in 0..cy {
+            for x in 0..cx {
+                self.move_to_new_position(x, y, area);
             }
         }
-        // If there is a conflict, two or more points moved to same pixel, don't drop conflicts around, because that would create really bad mess,
-        // Simply calculate new elements in the next calculation iteration, it gives better results without complications.
-        let mut all_some_none: Vec<Vec<Mutex<Option<DataPx>>>> = Vec::new();
-        for _ in 0..self.width {
-            let mut vy = Vec::new();
-            for _ in 0..self.height {
-                vy.push(Mutex::new(None));
-            }
-            all_some_none.push(vy);
-        }
-        // 2. calculate position for moving elements and resolve conflicts
-        let mut c_conflict = 0;
-        for px in px_for_move {
-            // translate [px,py] to [re,im]
-            let (x, y) = area.point_to_pixel(px.origin_re, px.origin_im);
-            let filled_already_o = tpx_at(&all_some_none, x, y).lock().unwrap().clone();
-            match filled_already_o {
-                // conflict
-                Some(conf) => {
-                    c_conflict += 1;
-                    if conf.has_worse_state_then(&px) {
-                        // Replace by element with better state
-                        // Better to delete the other one, then to drop it to other empty pixel.
-                        // That would cause problem with optimization, better calculate new and shiny pixel
-                        set_tpx_at(&all_some_none, x, y, px.clone());
-                    }
-                }
-                // Excellent, there is no conflict
-                None => {
-                    set_tpx_at(&all_some_none, x, y, px.clone());
-                }
+        now("2. move top right to center");
+        for y in 0..cy {
+            for x in (cx..self.width).rev() {
+                self.move_to_new_position(x, y, area);
             }
         }
-        // 3. drop moved elements to new positions and create new elements on positions where nothing was moved to
+        now("3. move bottom left to center");
+        for y in (cy..self.height).rev() {
+            for x in 0..cx {
+                self.move_to_new_position(x, y, area);
+            }
+        }
+        now("4. move bottom right to center");
+        for y in (cy..self.height).rev() {
+            for x in (cx..self.width).rev() {
+                self.move_to_new_position(x, y, area);
+            }
+        }
+        // Create new elements on positions where no px moved to
+        now("fill empty places");
         let mut c_moved = 0;
         let mut c_created = 0;
         for y in 0..self.height {
             for x in 0..self.width {
-                let mg_el_co = tpx_at(&all_some_none, x, y).lock().unwrap();
-                match &*mg_el_co {
-                    Some(cop) => {
-                        // to minimize conflict in next zoom in, must recalculate pixel centers for moved elements
-                        let re = area.screen_to_domain_re(x);
-                        let im = area.screen_to_domain_im(y);
-                        let mut cl = cop.clone();
-                        cl.origin_re = re;
-                        cl.origin_im = im;
-                        self.replace_px(x, y, cl);
-                        c_moved += 1;
+                let mut mo_px = self.mo_px_at(x as usize, y as usize);
+                if mo_px.is_none() {
+                    c_created += 1;
+                    let re = area.screen_to_domain_re(x);
+                    let im = area.screen_to_domain_im(y);
+                    if self.all_neighbors_finished_bad(x, y, is_mandelbrot) {
+                        // Calculation for some positions should be skipped as they are too far away form any long successful divergent position
+                        mo_px.replace(hibernated_deep_black(re, im));
+                    } else {
+                        mo_px.replace(active_new(re, im));
                     }
-                    None => {
-                        c_created += 1;
-                        let re = area.screen_to_domain_re(x);
-                        let im = area.screen_to_domain_im(y);
-                        if self.all_neighbors_finished_bad(x, y, is_mandelbrot) {
-                            // Calculation for some positions should be skipped as they are too far away form any long successful divergent position
-                            self.replace_px(x, y, hibernated_deep_black(re, im));
-                        } else {
-                            self.replace_px(x, y, active_new(re, im));
-                        }
-                    }
+                } else {
+                    c_moved += 1;
                 }
             }
         }
-        println!("to move:   {}", c_to_move);
-        println!("to drop:   {}", c_to_drop);
-        println!("conflicts: {}", c_conflict);
         println!("moved:     {}", c_moved);
         println!("created:   {}", c_created);
-        assert!(c_to_move > 0);
-        assert!(c_to_drop > 0);
         assert!(c_moved > 0);
         assert!(c_created > 0);
+    }
+
+    fn move_to_new_position(&self, x: usize, y: usize, area: &Area) {
+        let mut mo_px = self.mo_px_at(x, y);
+        let p = mo_px.as_mut().unwrap();
+        // There was already zoom in, the new area is smaller
+        if area.contains(p.origin_re, p.origin_im) {
+            // Element didn't move out of the smaller area
+            // new pixel position
+            let (nx, ny) = area.point_to_pixel(p.origin_re, p.origin_im);
+            if (x == nx) && (y == ny) {
+                // insignificant move within the same pixel
+            } else {
+                // move px to new position
+                self.move_px_to_new_position(nx, ny, p.to_owned());
+                // clean old position
+                *mo_px = None;
+            }
+        } else {
+            // clean position of elements which moved beyond area edges
+            *mo_px = None;
+        }
     }
 
     // Verify if any neighbor px,py finished well, long or at least too short.
     // This method identifies deep black convergent elements of Mandelbrot set interior.
     // Don't do any calculation for those.
-    fn all_neighbors_finished_bad(&mut self, x: usize, y: usize, is_mandelbrot: bool) -> bool {
+    fn all_neighbors_finished_bad(&self, x: usize, y: usize, is_mandelbrot: bool) -> bool {
         let neigh = NEIGHBOURS as i32;
         for a in -neigh..(neigh + 1) {
             for b in -neigh..(neigh + 1) {
                 let xx = x as i32 + a;
                 let yy = y as i32 + b;
-                if check_domain(xx, yy, self.width, self.height) {
-                    let oel = self.mpx_o_at(xx as usize, yy as usize);
-                    match oel {
-                        None => {
-                            // no element moved to this position
-                        }
-                        Some(ael) => {
-                            let el = ael.lock().unwrap();
-                            if is_mandelbrot {
-                                if el.is_finished_too_long() || el.is_hibernated() {
-                                    return false;
-                                }
-                            } else {
-                                if el.is_finished_success_any() || el.is_finished_too_short() {
-                                    return false;
-                                }
+                if (a != 0 || b != 0) && check_domain(xx, yy, self.width, self.height) {
+                    let mo_px = self.mo_px_at(xx as usize, yy as usize);
+                    if mo_px.is_some() {
+                        let px = mo_px.as_ref().unwrap();
+                        if is_mandelbrot {
+                            if px.is_finished_too_long() || px.is_hibernated() {
+                                return false;
+                            }
+                        } else {
+                            if px.is_finished_success_any() || px.is_finished_too_short() {
+                                return false;
                             }
                         }
                     }
@@ -445,15 +425,6 @@ impl DataImage {
         }
         true
     }
-}
-
-fn tpx_at(vec: &Vec<Vec<Mutex<Option<DataPx>>>>, x: usize, y: usize) -> &Mutex<Option<DataPx>> {
-    vec.get(x).unwrap().get(y).unwrap()
-}
-
-fn set_tpx_at(vec: &Vec<Vec<Mutex<Option<DataPx>>>>, x: usize, y: usize, px_ref: DataPx) {
-    let vy = vec.get(x).unwrap().to_owned();
-    vy[y].lock().unwrap().replace(px_ref.clone());
 }
 
 pub fn init_data_static() -> DataImage {
@@ -479,14 +450,14 @@ pub fn init(dynamic: bool) -> DataImage {
     }
 }
 
-fn init_domain(area: &Area) -> Vec<Vec<Arc<Mutex<DataPx>>>> {
+fn init_domain(area: &Area) -> Vec<Vec<Mutex<Option<DataPx>>>> {
     let mut vx = Vec::new();
     for x in 0..area.width_x {
         let mut vy = Vec::new();
         for y in 0..area.height_y {
             let origin_re = area.screen_to_domain_re(x);
             let origin_im = area.screen_to_domain_im(y);
-            vy.push(Arc::new(Mutex::new(data_px::init(origin_re, origin_im))));
+            vy.push(Mutex::new(Some(data_px::init(origin_re, origin_im))));
         }
         vx.push(vy);
     }
