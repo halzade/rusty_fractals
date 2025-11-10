5514f854e3231d83575ed5eb9570e18bb8002fee
lukas <lukas@halza.de>
2025-11-10 17:19:30 +0100

    initial commit np

---
+++ fractal_nebula_perfection/src/main.rs
@@ -0,0 +1,57 @@
+use rusty_fractals::application;
+use rusty_fractals::fractal::FractalCalculationType::StaticImageNebula;
+use rusty_fractals::fractal::OrbitType::Finite;
+use rusty_fractals::fractal::{FractalConfig, FractalMath};
+use rusty_fractals::mem::Mem;
+use rusty_fractals::palettes::PaletteName::{BlackToWhiteCircleUp, BlackWBWB, Nothing};
+use rusty_fractals::resolution_multiplier::ResolutionMultiplier::Single;
+
+pub struct NebulaPerfection {}
+
+impl FractalMath<Mem> for NebulaPerfection {
+    fn math(&self, m: &mut Mem, origin_re: f64, origin_im: f64) {
+        m.square();
+        m.plus(origin_re, origin_im);
+    }
+}
+
+fn main() {
+    let fractal_config = FractalConfig {
+        name: "NebulaPerfection",
+        fractal_calc_type: StaticImageNebula,
+
+        iteration_min: 42,
+        iteration_max: 1480,
+        resolution_multiplier: Single,
+        palette: BlackWBWB,
+
+        palette_zero: Nothing,
+        width_x: 400,
+        height_y: 800,
+        width_re: 0.5,
+        center_re: -1.25,
+        center_im: 0.0,
+
+        orbits: Finite,
+        update_max: 150,
+        update_min: 0,
+    };
+
+    application::execute(fractal_config, NebulaPerfection {});
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::NebulaPerfection;
+    use rusty_fractals::fractal::FractalMath;
+    use rusty_fractals::mem::Mem;
+
+    #[test]
+    fn test_math() {
+        let nebula = NebulaPerfection {};
+        let mut m = Mem { re: 0.0, im: 0.0 };
+        nebula.math(&mut m, 1.0, 0.1);
+        assert_eq!(m.re, 1.0);
+        assert_eq!(m.im, 0.1);
+    }
+}
