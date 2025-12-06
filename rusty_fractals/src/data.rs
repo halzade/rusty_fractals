b7a6ced968a32d71952623d7f8326518dffcd45b
lukas <lukas@halza.de>
2025-12-06 21:22:09 +0100

    add unit test

--- rusty_fractals/src/data.rs
+++ rusty_fractals/src/data.rs
@@ -32,10 +32,33 @@ pub fn init_from_data(file_name: &str) -> Vec<DataPx> {
         .collect()
 }
 
-pub fn save_data(file_name: &str, data: Vec<DataPx>) {
+pub fn save_data(file_name: &str, data: &Vec<DataPx>) {
     let mut writer = BufWriter::new(File::create(file_name).unwrap());
 
     let write_serializable: Vec<DataPxSer> = data.iter().map(|px| px.to_serializable()).collect();
 
     encode_into_std_write(&write_serializable, &mut writer, standard()).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::data_px;
+    use std::fs;
+
+    #[test]
+    fn test_save_and_load() {
+        let file_name = "test_data.bin";
+        let original = vec![data_px::init_trivial()];
+
+        // save
+        save_data(file_name, &original);
+        // load
+        let loaded = init_from_data(file_name);
+
+        assert!(fs::metadata(file_name).unwrap().is_file());
+        assert_eq!(original.len(), loaded.len());
+
+        fs::remove_file(file_name).unwrap();
+    }
 }
